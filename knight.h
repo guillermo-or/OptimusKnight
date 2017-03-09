22d2b8b22ac97d500c2bc950599b5bdeeda5190a
goretkin <gustavo.goretkin@gmail.com>
2017-03-09 10:13:00 -0500

    implement simple dynamics
    Endow `Move` with hash and equality. Start migrating Board.

--- knight.h
+++ knight.h
@@ -2,6 +2,9 @@
 #define KNIGHT_H
 
 #include <iostream>
+#include <set>
+#include <vector>
+#include <unordered_set>
 
 struct Move {
 	// against https://google.github.io/styleguide/cppguide.html#Access_Control
@@ -9,13 +12,48 @@ struct Move {
 	int col;
 };
 
+// naive hash of `Move`. Consider not putting this in std, but instead template containers with
+// this hash function.
+namespace std {
+	template <> struct hash<Move> {
+		size_t operator()(const Move& m) const {
+			return hash<int>()(m.row) ^ hash<int>()(m.col);
+		}
+	};
+}
+
+// I did not find an opinion on the Google style guide regarding member vs not member
+// operator overloading.
+// Here's one: http://stackoverflow.com/a/4421729/415404
+bool operator==(const Move& lhs, const Move& rhs ) {
+	return std::tie(lhs.row, lhs.col) == std::tie(rhs.row, rhs.col);
+	//return (lhs.row==rhs.row) && (lhs.col==rhs.col);
+}
+
+bool operator!=(const Move& lhs, const Move& rhs){ return !operator==(lhs,rhs); }
+
+
 std::ostream& operator<<(std::ostream& os, const Move& move) {
 	os << "Move(" << move.row << ", "<< move.col << ")";
 	return os;
 }
 
-struct Board {
-	int rows;
-	int cols;
-};
-#endif
+
+std::unordered_set <Move> dynamics(const Move& now) {
+	std::unordered_set <Move> neighbors;
+	std::vector< int > twos = {-2, 2};
+	std::vector< int > ones = {-1, 1};
+
+	// https://google.github.io/styleguide/cppguide.html#auto
+	for (auto two = twos.begin(); two != twos.end(); ++two) {
+		for (auto one = ones.begin(); one != ones.end(); ++one) {
+			Move m_rc = {now.row + *two, now.col + *one};
+			Move m_cr = {now.row + *one, now.col + *two};
+			neighbors.insert(m_rc);
+			neighbors.insert(m_cr);
+		}
+	}
+	return neighbors;
+}
+
+#endif /* KNIGHT_H_ */
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,4 +1,5 @@
 #include "../knight.h"
+#include "../board.h"
 #include <sstream>
 
 #include "gtest/gtest.h"
@@ -16,4 +17,26 @@ TEST(MoveTest, Repr)
 	Move m = {20, 30};
 	fake_cout << m;
 	EXPECT_EQ("Move(20, 30)", fake_cout.str());
+}
+
+TEST(MoveTest, Equality)
+{
+	Move a = {20, 30};
+	Move b = {20, 30};
+	Move c = {21, 30};
+	Move d = {20, 31};
+	EXPECT_TRUE(a == b);
+	EXPECT_TRUE(a != c);
+	EXPECT_TRUE(a != d);
+	EXPECT_TRUE(c != d);
+}
+
+TEST(DynamicsTest, NoBoundary)
+{
+	Move start = {4,4};
+	auto successors = dynamics(start);
+	std::unordered_set<Move> expected({
+		 {5, 6}, {6, 5}, {3, 6}, {6, 3}, {5, 2}, {2, 5}, {3, 2}, {2, 3}
+	});
+	EXPECT_EQ(expected, successors);
 }
